f01b9c7472f80d995edceeab5ada01b8d975569c
Amauri Carlos <amauricarlosdecarvalho@gmail.com>
2026-09-22 15:37:11 -0300

    Move ApiError/ErrorResponse

--- br.edu.fatec.delivery/src/main/java/br/edu/fatec/delivery/cliente/dto/ApiError.java
+++ br.edu.fatec.delivery/src/main/java/br/edu/fatec/delivery/cliente/dto/ApiError.java
@@ -1,4 +1,4 @@
-package br.edu.fatec.delivery.cliente.exception;
+package br.edu.fatec.delivery.cliente.dto;
 
 import java.time.Instant;
 
--- br.edu.fatec.delivery/src/main/java/br/edu/fatec/delivery/cliente/dto/ErrorResponse.java
+++ br.edu.fatec.delivery/src/main/java/br/edu/fatec/delivery/cliente/dto/ErrorResponse.java
@@ -1,4 +1,4 @@
-package br.edu.fatec.delivery.cliente.exception;
+package br.edu.fatec.delivery.cliente.dto;
 
 public record ErrorResponse(
         ApiError erro
